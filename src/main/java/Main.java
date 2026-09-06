4d8e2d716d00d940b72ae8ee1ec889b293cd2393
NikitaAvrn <mel16bit@gmail.com>
2026-09-06 17:09:45 +0300

    feat(integrate): integrate user service & ui to app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import userservice.ui.UserConsoleUI;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello and welcome!");
+        UserConsoleUI ui = new UserConsoleUI();
+        ui.start();
     }
 }
